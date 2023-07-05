1a0ffe96f12ce4c1ac5e846b5237514d6a4a57be
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2023-07-04 23:03:42 -0300

    initial commit

---
+++ checks/rstats/testthat/setup.R
@@ -0,0 +1,3 @@
+library(data.table); library(frictionless)
+
+ppag <- read_package("datapackages/ppag/datapackage.json")
---
+++ checks/rstats/testthat/test_valor_total_anual.R
@@ -0,0 +1,17 @@
+test_that("Programa possui apenas uma área temática", {
+  #' @details
+  #' Verificar se o valor total do orçamento (orçamento fiscal e orçamento de 
+  #' investimento das empresas controladas) coincide com a projeção do PPAG 
+  #' para o ano seguinte
+  
+  base_qdd_fiscal <- fread("datapackages/loa/base_qdd_fiscal.csv")
+  
+  acoes_planejamento <- read_resource(ppag, "acoes_planejamento") |> as.data.table()
+  acoes_planejamento <- fread("datapackages/ppag/acoes_planejamento.csv")
+  
+  sisor <- base_qdd_fiscal[, sum(vl_loa_desp)]
+  sigplan <- acoes_planejamento[is_deleted_acao == FALSE & 
+                                identificador_tipo_acao_cod %in% c(1, 2, 4, 7, 9), 
+                                sum(vr_meta_orcamentaria_ano0)]
+  expect_equal(sisor, sigplan)
+})
