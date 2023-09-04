0ac98ea033b4a2f85978935f1465def983c62050
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2023-09-04 18:43:59 -0300

    atualiza dpm para uso do data.yaml

--- checks/rstats/testthat/test_valor_total_anual.R
+++ checks/rstats/testthat/test_valor_total_anual.R
@@ -4,12 +4,12 @@ test_that("Total do orçamento fiscal e investimento SIGPLAN vs SISOR", {
   #' investimento das empresas controladas) coincide com a projeção do PPAG 
   #' para o ano seguinte
   
-  base_qdd_fiscal <- fread(here("datapackages/loa/base_qdd_fiscal.csv"))
+  base_qdd_fiscal <- fread(here("datapackages/loa/data/base_qdd_fiscal.csv"))
   
   acoes_planejamento <- read_resource(ppag, "acoes_planejamento") |> as.data.table()
   acoes_planejamento <- fread(here("datapackages", "ppag", "acoes_planejamento.csv"))
   
-  sisor <- base_qdd_fiscal[, sum(vl_loa_desp)]
+  sisor <- base_qdd_fiscal[, sum(VL_LOA_DESP)]
   sigplan <- acoes_planejamento[is_deleted_acao == FALSE & 
                                 identificador_tipo_acao_cod %in% c(1, 2, 4, 7, 9), 
                                 sum(vr_meta_orcamentaria_ano0)]
