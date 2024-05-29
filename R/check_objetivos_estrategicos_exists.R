d2d7126e0e0531c6d662caa21fb938cce333e1fe
carloshob <carloshenrique.ob@gmail.com>
2024-05-29 20:36:20 -0300

    implementa versao inicial da funcao para teste dos objetivos estrategicos

---
+++ R/check_objetivos_estrategicos_exists.R
@@ -0,0 +1,22 @@
+#' Verifica se cada programa possui pelo menos um objetivo estratégico
+#'
+#'  
+#' 
+#'
+#' @export
+check_objetivos_estrategicos_exist <- function(programas_planejamento, output = FALSE, stop_on_failure = FALSE) {
+
+  df <- programas_planejamento |> 
+    filter(is_deleted_programa == FALSE) |> 
+    distinct(programa_cod, programa_desc, objetivo_estrategico_cod, objetivo_estrategico_desc) |> 
+    summarize(
+      objetivo_estrategico_count = sum(!is.na(objetivo_estrategico_cod) & objetivo_estrategico_cod = 3), 
+      .by = c("programa_cod", "programa_desc")
+    )
+  
+  report <- check_that(df, objetivo_estrategico_count > 0)
+  
+  check_result(df, report, stop_on_failure = stop_on_failure, output = output)
+  
+    
+}
