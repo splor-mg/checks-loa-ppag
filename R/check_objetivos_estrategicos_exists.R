13781cd04e1bc53514804242e0a5f53f1fc72c80
carloshob <carloshenrique.ob@gmail.com>
2024-06-03 10:58:24 -0300

    implementa funcao chekc_objetivos_estrategico_exists

--- R/check_objetivos_estrategicos_exists.R
+++ R/check_objetivos_estrategicos_exists.R
@@ -10,13 +10,11 @@ check_objetivos_estrategicos_exists <- function(programas_planejamento, output =
     filter(is_deleted_programa == FALSE) |> 
     distinct(programa_cod, programa_desc, objetivo_estrategico_cod, objetivo_estrategico_desc) |> 
     summarize(
-      objetivo_estrategico_count = sum(!is.na(objetivo_estrategico_cod) & objetivo_estrategico_cod = 3), 
+      objetivo_estrategico_count = sum(is.na(objetivo_estrategico_cod) | objetivo_estrategico_cod == 3), 
       .by = c("programa_cod", "programa_desc")
     )
   
-  report <- check_that(df, objetivo_estrategico_count > 0)
+  report <- check_that(df, objetivo_estrategico_count == 0)
   
   check_result(df, report, stop_on_failure = stop_on_failure, output = output)
-  
-    
 }
