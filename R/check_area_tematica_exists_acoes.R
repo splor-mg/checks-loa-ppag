86dc3dda3627b814bb7388b55315f8fd33b98359
carloshob <carloshenrique.ob@gmail.com>
2024-08-22 17:10:40 -0300

    uniformiza identacao e comentarios inicial

--- R/check_area_tematica_exists_acoes.R
+++ R/check_area_tematica_exists_acoes.R
@@ -1,4 +1,4 @@
-#' Verifica se cada programa possui uma única área temática - acoes_planejamento
+#' Verifica se cada programa possui apenas uma área temática - acoes_planejamento
 #'
 #' É obrigatório a existência de uma área por programa (ie. _one and only one_).
 #'
@@ -6,28 +6,34 @@
 check_area_tematica_exists_acoes <- function(acoes_planejamento,
                                              stop_on_failure = FALSE,
                                              output = FALSE,
-                                             json_outfile = NULL, log_level = "ERROR",
+                                             json_outfile = NULL, 
+                                             log_level = "ERROR",
                                              msg_template = NULL
-                                             ) {
+                                            ) {
   df <- acoes_planejamento |>
     filter(is_deleted_acao == FALSE) |>
-    distinct(
-      programa_cod,
-      programa_desc,
-      area_tematica_cod,
-      area_tematica_desc
-    ) |>
+    distinct(programa_cod,
+             programa_desc,
+             area_tematica_cod,
+             area_tematica_desc
+             ) |>
     group_by(programa_cod) |>
     summarize(area_tematica_cod_count = sum(!is.na(area_tematica_cod)))
 
   report <- check_that(df, area_tematica_cod_count == 1)
 
-  default_message = "Na base acoes_planejamento o programa {programa_cod} possui {area_tematica_cod_count} área(s) temática(s). Cada programa deve possuir uma e somente uma área temática."
+  default_message = "Na base acoes_planejamento o programa {programa_cod}",
+                    "possui {area_tematica_cod_count} área(s) temática(s).",
+                    "Cada programa deve possuir uma e somente uma área temática."
   
   # prioritize the parameter error message if used
   msg_template = msg_template %||% default_message
   
-  
-  check_result(df, report, stop_on_failure = stop_on_failure, output = output, 
-               json_outfile = json_outfile, log_level = log_level, msg_template = msg_template)
+  check_result(df, report,
+               stop_on_failure = stop_on_failure,
+               output = output, 
+               json_outfile = json_outfile,
+               log_level = log_level,
+               msg_template = msg_template
+               )
 }
--- R/check_area_tematica_exists_programas.R
+++ R/check_area_tematica_exists_programas.R
@@ -3,22 +3,37 @@
 #' É obrigatório a existência de uma área por programa (ie. _one and only one_).
 #'
 #' @export
-check_area_tematica_exists_programas <- function(programas_planejamento, stop_on_failure = FALSE, output = FALSE,
-                                                 json_outfile = NULL, log_level = "ERROR",
-                                                 msg_template = NULL) {
+check_area_tematica_exists_programas <- function(programas_planejamento,
+                                                 stop_on_failure = FALSE,
+                                                 output = FALSE,
+                                                 json_outfile = NULL,
+                                                 log_level = "ERROR",
+                                                 msg_template = NULL
+                                                 ) {
   df <- programas_planejamento |>
     filter(is_deleted_programa == FALSE) |>
-    distinct(programa_cod, programa_desc, area_tematica_cod, area_tematica_desc) |>
+    distinct(programa_cod,
+             programa_desc,
+             area_tematica_cod,
+             area_tematica_desc
+             ) |>
     group_by(programa_cod) |>
     summarize(area_tematica_cod_count = sum(!is.na(area_tematica_cod)))
 
   report <- check_that(df, area_tematica_cod_count == 1)
   
-  default_message = "Na base programas_planejamento o programa {programa_cod} possui {area_tematica_cod_count} área(s) temática(s). Cada programa deve possuir uma e somente uma área temática."
+  default_message = "Na base programas_planejamento o programa {programa_cod}",
+                    "possui {area_tematica_cod_count} área(s) temática(s). ",
+                    "Cada programa deve possuir uma e somente uma área temática."
   
   # prioritize the parameter error message if used
   msg_template = msg_template %||% default_message
 
-  check_result(df, report, stop_on_failure = stop_on_failure, output = output, 
-               json_outfile = json_outfile, log_level = log_level, msg_template = msg_template)
+  check_result(df, report,
+               stop_on_failure = stop_on_failure,
+               output = output,
+               json_outfile = json_outfile,
+               log_level = log_level,
+               msg_template = msg_template
+               )
 }
