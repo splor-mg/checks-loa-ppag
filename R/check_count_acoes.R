e70c72a18177f353fbf7f6dbc11ea68822d71d22
carloshob <carloshenrique.ob@gmail.com>
2024-08-26 11:56:13 -0300

    ck_cnt_acoes - atualiza mensagem de erro

--- R/check_count_acoes.R
+++ R/check_count_acoes.R
@@ -7,30 +7,58 @@ check_count_acoes <- function(acoes_planejamento,
                               localizadores_todos_planejamento,
                               output = FALSE,
                               stop_on_failure = FALSE,
-                              json_outfile = NULL, log_level = "ERROR",
+                              json_outfile = NULL,
+                              log_level = "ERROR",
                               msg_template = NULL) {
   x <- acoes_planejamento |>
     filter(is_deleted_acao == FALSE) |>
-    distinct(uo_acao_cod, uo_acao_nome, acao_cod, acao_desc) |>
+    distinct(uo_acao_cod,
+             uo_acao_nome,
+             acao_cod,
+             acao_desc
+             ) |>
     rename(acoes = acao_desc)
 
   y <- localizadores_todos_planejamento |>
     filter(is_deleted_acao == FALSE) |>
-    distinct(uo_acao_cod, uo_acao_nome, acao_cod, acao_desc) |>
+    distinct(uo_acao_cod,
+             uo_acao_nome,
+             acao_cod,
+             acao_desc
+             ) |>
     rename(localizadores = acao_desc)
 
-  df <- merge(x, y, by = c("uo_acao_cod", "acao_cod"), all = TRUE)
+  df <- merge(x, y, by = c("uo_acao_cod", "acao_cod"),
+                    all = TRUE)
 
   report <- check_that(df, acoes == localizadores)
   
-  default_message = "Foram encontrados erros no teste."
+  default_message <- ifelse(is.na(localizadores),
+                            paste0(
+                              "A ação {acao_cod}, da UO {uo_acao_cod} ",
+                              "consta na base acoes_planejamento, porém não ",
+                              "na base localizadores_todos_planejamento."),
+                            ifelse(is.na(df$acoes),
+                              paste0(
+                               "A ação {acao_cod}, da UO {uo_acao_cod} ",
+                               "consta na base localizadores_todos_planejamento, ",
+                               "porém não na base acoes_planejamento."),
+                              paste0(
+                               "A ação {acao_cod}, da UO {uo_acao_cod} ",
+                               "apresenta descrição diferente nas bases ",
+                               "localizadores_todos_planejamento e acoes_planejamento.")
+                            )
+                     )
   
   # prioritize the parameter error message if used
   msg_template = msg_template %||% default_message
 
-  check_result(
-    df, report,
-    stop_on_failure = stop_on_failure, output = output, summary = count(df), 
-    json_outfile = json_outfile, log_level = log_level, msg_template = msg_template
+  check_result(df, report,
+               stop_on_failure = stop_on_failure,
+               output = output,
+               summary = count(df), 
+               json_outfile = json_outfile,
+               log_level = log_level,
+               msg_template = msg_template
   )
 }
