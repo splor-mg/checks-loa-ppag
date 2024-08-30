0b05b3979b560e9d89c4b76fc8cc86f9edf9517b
carloshob <carloshenrique.ob@gmail.com>
2024-08-30 19:24:17 -0300

    ck-detalhamento-obras-investimento - altera mensagem do check

--- R/check_detalhamento_obras_orcam_investimento.R
+++ R/check_detalhamento_obras_orcam_investimento.R
@@ -41,7 +41,11 @@ check_detalhamento_obras_orcam_investimento <- function(base_qdd_investimento,
 
   report <- df |> check_that(vlr_loa_desp_invest == vlr_outros_ano0)
   
-  default_message = "Foram encontrados erros no teste."
+  default_message = paste0(
+                    "A ação {acao_cod}, na UO {uo_cod}, função-subfunção {funcao_cod}-{subfuncao_cod}, ",
+                    "programa {programa_cod} e iag {iag_cod} está com valores diferentes entre as bases, ",
+                    "R$ {vlr_loa_desp_invest} na qdd investimento e R$ {vlr_outros_ano0} na detalhamento de obras."
+                    )
   
   # prioritize the parameter error message if used
   msg_template = msg_template %||% default_message
