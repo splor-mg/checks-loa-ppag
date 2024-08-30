ca9529ae0a1a863ca9313b360321aa7c9fa38551
carloshob <carloshenrique.ob@gmail.com>
2024-08-30 19:17:13 -0300

    ck-detalhamento-obras-plurianual - altera mensagem do check

--- R/check_detalhamento_obras_orcam_fiscal_outros_plurianual.R
+++ R/check_detalhamento_obras_orcam_fiscal_outros_plurianual.R
@@ -45,7 +45,7 @@ check_detalhamento_obras_orcam_fiscal_outros_plurianual <- function(base_qdd_plu
                              vlr_loa_desp_ano3 >= vlr_outros_ano3
                             )
   
-  default_message = "Foram encontrados erros no teste."
+  default_message = "A ação {acao_cod}, na UO {uo_cod}, função-subfunção {funcao_cod}-{subfuncao_cod}, programa {programa_cod} e iag {iag_cod} está com valor na base qdd plurianual R$ {ifelse(vlr_loa_desp_ano0 < vlr_outros_ano0, vlr_loa_desp_ano0, ifelse(vlr_loa_desp_ano1 < vlr_outros_ano1, vlr_loa_desp_ano1, ifelse(vlr_loa_desp_ano2 < vlr_outros_ano2, vlr_loa_desp_ano2, vlr_loa_desp_ano3)))} menor do que na base detalhamento de obras R$ {ifelse(vlr_loa_desp_ano0 < vlr_outros_ano0, vlr_outros_ano0, ifelse(vlr_loa_desp_ano1 < vlr_outros_ano1, vlr_outros_ano1, ifelse(vlr_loa_desp_ano2 < vlr_outros_ano2, vlr_outros_ano2, vlr_outros_ano3)))}."
   
   # prioritize the parameter error message if used
   msg_template = msg_template %||% default_message
