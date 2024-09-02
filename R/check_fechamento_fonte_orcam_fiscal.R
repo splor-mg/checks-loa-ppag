45db5a642b19ab472e8548fc2a822de881113e1e
carloshob <carloshenrique.ob@gmail.com>
2024-09-02 17:43:49 -0300

    ck_fecham_fonte_fiscal - altera documentação e identação

--- R/check_fechamento_fonte_orcam_fiscal.R
+++ R/check_fechamento_fonte_orcam_fiscal.R
@@ -1,20 +1,36 @@
 #' Fechamento receita e despesa por fonte do orçamento fiscal
 #'
+#'
+#'
+#'
+#'
 #' @export
-check_fechamento_fonte_orcam_fiscal <- function(base_orcam_receita_fiscal, base_qdd_fiscal, stop_on_failure = FALSE, output = FALSE,
-                                                json_outfile = NULL, log_level = "ERROR",
-                                                msg_template = NULL) {
+check_fechamento_fonte_orcam_fiscal <- function(base_orcam_receita_fiscal,
+                                                base_qdd_fiscal,
+                                                stop_on_failure = FALSE,
+                                                output = FALSE,
+                                                json_outfile = NULL,
+                                                log_level = "ERROR",
+                                                msg_template = NULL
+                                                ) {
   key <- c("fonte_cod")
 
   x <- base_orcam_receita_fiscal |>
-    aggregate("vlr_loa_rec$", by = key)
+       aggregate("vlr_loa_rec$",
+                 by = key
+                 )
 
   y <- base_qdd_fiscal |>
-    aggregate("vlr_loa_desp$",
-      by = key
-    )
+       aggregate("vlr_loa_desp$",
+                 by = key
+                 )
 
-  df <- merge(x, y, by = key, all = TRUE) |> as_accounting()
+  df <- merge(x, y,
+              by = key,
+              all = TRUE
+              ) |> 
+        as_accounting()
+  
   report <- df |> check_that(vlr_loa_rec == vlr_loa_desp)
   
   default_message = "Foram encontrados erros no teste."
@@ -22,6 +38,11 @@ check_fechamento_fonte_orcam_fiscal <- function(base_orcam_receita_fiscal, base_
   # prioritize the parameter error message if used
   msg_template = msg_template %||% default_message
   
-  check_result(df, report, stop_on_failure = stop_on_failure, output = output,
-               json_outfile = json_outfile, log_level = log_level, msg_template = msg_template)
+  check_result(df, report,
+               stop_on_failure = stop_on_failure,
+               output = output,
+               json_outfile = json_outfile,
+               log_level = log_level,
+               msg_template = msg_template
+               )
 }
