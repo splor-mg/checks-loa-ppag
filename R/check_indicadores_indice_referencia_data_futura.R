a01b9e3fdd31fb845c86b015a0f03ffdd98c88ce
carloshob <carloshenrique.ob@gmail.com>
2024-09-03 15:35:46 -0300

    ck_indicadores_referencia_futura - altera documentação e identação

--- R/check_indicadores_indice_referencia_data_futura.R
+++ R/check_indicadores_indice_referencia_data_futura.R
@@ -2,14 +2,21 @@
 #'
 #' A data futura ocorre quando dt_apuracao >= updated_at
 #'
+#'
+#'
 #' @export
-check_indicadores_indice_referencia_data_futura <- function(indicadores_planejamento, output = FALSE, stop_on_failure = FALSE,
-                                                            json_outfile = NULL, log_level = "ERROR",
-                                                            msg_template = NULL) {
+check_indicadores_indice_referencia_data_futura <- function(indicadores_planejamento,
+                                                            output = FALSE,
+                                                            stop_on_failure = FALSE,
+                                                            json_outfile = NULL,
+                                                            log_level = "ERROR",
+                                                            msg_template = NULL
+                                                            ) {
   df <- indicadores_planejamento |>
-    filter(is_deleted_programa == FALSE &
-      is_deleted_indicador == FALSE &
-      is_em_apuracao_indice_de_referencia == FALSE)
+        filter(is_deleted_programa == FALSE &
+               is_deleted_indicador == FALSE &
+               is_em_apuracao_indice_de_referencia == FALSE
+               )
 
   # operador menor ou igual pois o indice de referencia pode ter sido
   # calculado no dia da atualizacao (eg. Indicador "SUCESSO DO PLANEJAMENTO ORÇAMENTÁRIO")
@@ -20,6 +27,11 @@ check_indicadores_indice_referencia_data_futura <- function(indicadores_planejam
   # prioritize the parameter error message if used
   msg_template = msg_template %||% default_message
 
-  check_result(df, report, stop_on_failure = stop_on_failure, output = output,
-               json_outfile = json_outfile, log_level = log_level, msg_template = msg_template)
+  check_result(df, report,
+               stop_on_failure = stop_on_failure,
+               output = output,
+               json_outfile = json_outfile,
+               log_level = log_level,
+               msg_template = msg_template
+               )
 }
