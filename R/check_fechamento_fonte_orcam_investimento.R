e8e1dbb9d1cfdc08977d3d4d8afc93a6af5f64b8
carloshob <carloshenrique.ob@gmail.com>
2024-09-03 11:26:07 -0300

    ck_fecham_fonte_investimento - altera mensagem do check

--- R/check_fechamento_fonte_orcam_investimento.R
+++ R/check_fechamento_fonte_orcam_investimento.R
@@ -40,7 +40,7 @@ check_fechamento_fonte_orcam_investimento <- function(base_orcam_receita_investi
 
   report <- df |> check_that(vlr_loa_rec_invest  == vlr_loa_desp_invest)
   
-  default_message = "Foram encontrados erros no teste."
+  default_message = "A Fonte de Recurso {fonte_cod} está com os valores totais de receita e despesa diferentes nas bases. R$ {vlr_loa_rec_invest} na base receita de investimento, e R$ {vlr_loa_desp_invest} na qdd investimento. A diferença é de R$ {ifelse(is.na(vlr_loa_rec_invest) | vlr_loa_rec_invest == 0, vlr_loa_desp_invest, ifelse(is.na(vlr_loa_desp_invest) | vlr_loa_desp_invest == 0, vlr_loa_rec_invest, vlr_loa_rec_invest - vlr_loa_desp_invest))}."
   
   # prioritize the parameter error message if used
   msg_template = msg_template %||% default_message
