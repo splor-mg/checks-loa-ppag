c7c8ce2d96a7568c390f3cad534ddc056702c03a
carloshob <carloshenrique.ob@gmail.com>
2024-09-03 17:25:06 -0300

    ck_indicadores_indice_referencia_zerado - altera documentação e identação

--- R/check_indicadores_indice_referencia_zerado.R
+++ R/check_indicadores_indice_referencia_zerado.R
@@ -1,13 +1,22 @@
 #' Verifica se indicadores não possuem índice de referência zerados
 #'
+#' 
+#'
+#'
+#'
 #' @export
-check_indicadores_indice_referencia_zerado <- function(indicadores_planejamento, output = FALSE, stop_on_failure = FALSE,
-                                                       json_outfile = NULL, log_level = "ERROR",
-                                                       msg_template = NULL) {
+check_indicadores_indice_referencia_zerado <- function(indicadores_planejamento,
+                                                       output = FALSE,
+                                                       stop_on_failure = FALSE,
+                                                       json_outfile = NULL,
+                                                       log_level = "ERROR",
+                                                       msg_template = NULL
+                                                       ) {
   df <- indicadores_planejamento |>
-    filter(is_deleted_programa == FALSE &
-      is_deleted_indicador == FALSE &
-      is_em_apuracao_indice_de_referencia == FALSE)
+        filter(is_deleted_programa == FALSE &
+               is_deleted_indicador == FALSE &
+               is_em_apuracao_indice_de_referencia == FALSE
+               )
 
   report <- check_that(df, indice_de_referencia != 0)
   
@@ -16,6 +25,11 @@ check_indicadores_indice_referencia_zerado <- function(indicadores_planejamento,
   # prioritize the parameter error message if used
   msg_template = msg_template %||% default_message
 
-  check_result(df, report, stop_on_failure = stop_on_failure, output = output,
-               json_outfile = json_outfile, log_level = log_level, msg_template = msg_template)
+  check_result(df, report,
+               stop_on_failure = stop_on_failure,
+               output = output,
+               json_outfile = json_outfile,
+               log_level = log_level,
+               msg_template = msg_template
+               )
 }
