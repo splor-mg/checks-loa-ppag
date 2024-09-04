018d6ae3342554089453fea4eb5f01dd331d5b5c
carloshob <carloshenrique.ob@gmail.com>
2024-09-04 14:30:03 -0300

    ck_intra_detalhamento - altera documentação e identação

--- R/check_intra_detalhamento.R
+++ R/check_intra_detalhamento.R
@@ -3,22 +3,30 @@
 #' Verifica se as operações intra-orçamentárias registradas na
 #' base intraorçamentária de repasse estão adequadamente detalhadas no orçamento.
 #'
+#'
 #' @export
-check_intra_detalhamento <- function(base_intra_orcamentaria_detalhamento, stop_on_failure = FALSE, output = FALSE,
-                                     json_outfile = NULL, log_level = "ERROR",
-                                     msg_template = NULL) {
+check_intra_detalhamento <- function(base_intra_orcamentaria_detalhamento,
+                                     stop_on_failure = FALSE,
+                                     output = FALSE,
+                                     json_outfile = NULL,
+                                     log_level = "ERROR",
+                                     msg_template = NULL
+                                     ) {
   df <- base_intra_orcamentaria_detalhamento
-  report <- df |> check_that(vlr_recebido == vlr_detalhado)
+  
+  report <- check_that(df, vlr_recebido == vlr_detalhado)
   
   default_message = "Foram encontrados erros no teste."
   
   # prioritize the parameter error message if used
   msg_template = msg_template %||% default_message
   
-  check_result(base_intra_orcamentaria_detalhamento, report,
-    stop_on_failure = stop_on_failure, 
-    output = output,
-    summary = aggregate(df, "vlr"),
-    json_outfile = json_outfile, log_level = log_level, msg_template = msg_template
-  )
+  check_result(df, report,
+               stop_on_failure = stop_on_failure, 
+               output = output,
+               summary = aggregate(df, "vlr"),
+               json_outfile = json_outfile,
+               log_level = log_level,
+               msg_template = msg_template
+               )
 }
