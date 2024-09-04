a9d337290d9d8375f6996d2ca85eb611d62cc009
carloshob <carloshenrique.ob@gmail.com>
2024-09-04 06:08:12 -0300

    ck_indicadores_justificativa_previsao_apuracao - altera documentação e identação

--- R/check_indicadores_justificativa_previsao_em_apuracao.R
+++ R/check_indicadores_justificativa_previsao_em_apuracao.R
@@ -2,33 +2,41 @@
 #'
 #' Somente são aceitas como válidas justificativas acima de 50 caracteres
 #'
+#'
+#'
 #' @export
-check_indicadores_justificativa_previsao_em_apuracao <- function(
-    indicadores_planejamento,
-    output = FALSE,
-    stop_on_failure = FALSE,
-    json_outfile = NULL, log_level = "ERROR",
-    msg_template = NULL) {
+check_indicadores_justificativa_previsao_em_apuracao <- function(indicadores_planejamento,
+                                                                 output = FALSE,
+                                                                 stop_on_failure = FALSE,
+                                                                 json_outfile = NULL,
+                                                                 log_level = "ERROR",
+                                                                 msg_template = NULL
+                                                                 ) {
   df <- indicadores_planejamento |>
-    filter(is_deleted_programa == FALSE &
-      is_deleted_indicador == FALSE)
+        filter(is_deleted_programa == FALSE &
+               is_deleted_indicador == FALSE
+               )
 
-  report <- check_that(
-    df,
-    if (is.na(previsao_para_ano0) |
-      is.na(previsao_para_ano1) |
-      is.na(previsao_para_ano2) |
-      is.na(previsao_para_ano3)) 
-      !is.na(justificativa_status_apuracao_previsoes) &
-        (stringr::str_length(justificativa_status_apuracao_previsoes) > 50)
-    
-  )
+  report <- check_that(df,
+                       if (is.na(previsao_para_ano0) |
+                           is.na(previsao_para_ano1) |
+                           is.na(previsao_para_ano2) |
+                           is.na(previsao_para_ano3)
+                           ) 
+                           !is.na(justificativa_status_apuracao_previsoes) &
+                           (stringr::str_length(justificativa_status_apuracao_previsoes) > 50)
+                       )
   
   default_message = "Foram encontrados erros no teste."
   
   # prioritize the parameter error message if used
   msg_template = msg_template %||% default_message
 
-  check_result(df, report, stop_on_failure = stop_on_failure, output = output,
-               json_outfile = json_outfile, log_level = log_level, msg_template = msg_template)
+  check_result(df, report,
+               stop_on_failure = stop_on_failure,
+               output = output,
+               json_outfile = json_outfile,
+               log_level = log_level,
+               msg_template = msg_template
+               )
 }
