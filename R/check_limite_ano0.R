22a5e96143fd9d09e3990ce1af2c4b3e358fd474
carloshob <carloshenrique.ob@gmail.com>
2024-09-04 15:59:07 -0300

    ck_limite_ano0 - altera documentação e identação

--- R/check_limite_ano0.R
+++ R/check_limite_ano0.R
@@ -1,19 +1,32 @@
 #' Valor Limite anual = Valor Utilizado + Valor Transferido
 #'
+#'
+#'
+#'
+#'
 #' @export
-check_limite_ano0 <- function(base_limite_cota, stop_on_failure = FALSE, output = FALSE,
-                              json_outfile = NULL, log_level = "ERROR",
-                              msg_template = NULL) {
-  df <- base_limite_cota |> as_accounting()
-  report <- df |> check_that(
-    vlr_limite_ano0 == vlr_utilizado_ano0 + vlr_transferido
-  )
+check_limite_ano0 <- function(base_limite_cota,
+                              stop_on_failure = FALSE,
+                              output = FALSE,
+                              json_outfile = NULL,
+                              log_level = "ERROR",
+                              msg_template = NULL
+                              ) {
+  df <- base_limite_cota |>
+        as_accounting()
+  
+  report <- check_that(df, vlr_limite_ano0 == vlr_utilizado_ano0 + vlr_transferido)
   
   default_message = "Foram encontrados erros no teste."
   
   # prioritize the parameter error message if used
   msg_template = msg_template %||% default_message
   
-  check_result(df, report, stop_on_failure = stop_on_failure, output = output,
-               json_outfile = json_outfile, log_level = log_level, msg_template = msg_template)
+  check_result(df, report,
+               stop_on_failure = stop_on_failure,
+               output = output,
+               json_outfile = json_outfile,
+               log_level = log_level,
+               msg_template = msg_template
+               )
 }
