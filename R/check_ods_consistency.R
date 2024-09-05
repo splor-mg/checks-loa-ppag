ac26833ca269176efe8f72e004d436619cba3f4b
carloshob <carloshenrique.ob@gmail.com>
2024-09-05 10:12:51 -0300

    ck_ods_consistency - altera documentação e identação

--- R/check_ods_consistency.R
+++ R/check_ods_consistency.R
@@ -3,18 +3,29 @@
 #' Se a opção "ODS 18 - Não Possui ODS" foi selecionada, não faz sentido 
 #' que outro ODS também seja selecionado
 #'
+#'
 #' @export
-check_ods_consistency <- function(programas_planejamento, output = FALSE, stop_on_failure = FALSE,
-                                  json_outfile = NULL, log_level = "ERROR",
-                                  msg_template = NULL) {
+check_ods_consistency <- function(programas_planejamento,
+                                  output = FALSE,
+                                  stop_on_failure = FALSE,
+                                  json_outfile = NULL,
+                                  log_level = "ERROR",
+                                  msg_template = NULL
+                                  ) {
   
   PROGRAMAS <- programas_planejamento |> 
-    filter(ods_titulo == "18 - Não Possui Objetivo de Desenvolvimento Sustentável") |> 
-    pull(programa_cod)
+               filter(ods_titulo == "18 - Não Possui Objetivo de Desenvolvimento Sustentável") |> 
+               pull(programa_cod)
   
   df <- programas_planejamento |> 
-    filter(is_deleted_programa == FALSE & programa_cod %in% PROGRAMAS) |> 
-    distinct(programa_cod, programa_desc, ods_titulo)
+        filter(is_deleted_programa == FALSE &
+               programa_cod %in%
+               PROGRAMAS
+               ) |> 
+        distinct(programa_cod,
+                 programa_desc,
+                 ods_titulo
+                 )
     
   report <- check_that(df, is_unique(programa_cod))
   
@@ -23,6 +34,11 @@ check_ods_consistency <- function(programas_planejamento, output = FALSE, stop_o
   # prioritize the parameter error message if used
   msg_template = msg_template %||% default_message
   
-  check_result(df, report, stop_on_failure = stop_on_failure, output = output,
-               json_outfile = json_outfile, log_level = log_level, msg_template = msg_template)  
+  check_result(df, report,
+               stop_on_failure = stop_on_failure,
+               output = output,
+               json_outfile = json_outfile,
+               log_level = log_level,
+               msg_template = msg_template
+               )  
 }
