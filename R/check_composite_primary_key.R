5e1af90009c0b85fe3e63b091c36e6d0672a59e7
hslinhares <henrique.s.linhares@gmail.com>
2024-09-05 11:39:57 -0300

    refatora a função a deixando com menos linhas

--- R/check_composite_primary_key.R
+++ R/check_composite_primary_key.R
@@ -9,16 +9,16 @@ check_composite_primary_key  <- function(siplan){
   )
   
   df_list <- list (
-    programas = sigplan$programas_planejamento|> 
-      mutate(pkey = paste(uo_programa_cod, programa_cod, is_deleted_programa, objetivo_estrategico_cod, diretriz_estrategica_cod, ods_titulo, sep = '|')),
-    acoes = sigplan$acoes_planejamento |> 
-      mutate(pkey = paste(uo_acao_cod, acao_cod, is_deleted_acao, sep = '|')),
-    localizadores = sigplan$localizadores_todos_planejamento |>
-      mutate(pkey = paste(uo_acao_cod, acao_cod, is_deleted_acao, localizador_cod, sep = '|')),
-    indicadores = sigplan$indicadores_planejamento |>
-      mutate(pkey = paste(programa_cod, is_deleted_programa, indicador, is_deleted_indicador, sep = '|'))
+    programas = sigplan$programas_planejamento,
+    acoes = sigplan$acoes_planejamento,
+    localizadores = sigplan$localizadores_todos_planejamento,
+    indicadores = sigplan$indicadores_planejamento
   )
   
+  for (name in names(df_list)) {
+    df_list[[name]][, pkey := do.call(paste, c(.SD, sep = "|")), .SDcols = pkey_list[[name]]]
+  }
+
   rule_list <- list(
     rule_programas <- validate::validator(
       is_complete(uo_programa_cod, programa_cod, is_deleted_programa, objetivo_estrategico_cod, diretriz_estrategica_cod, ods_titulo), 
