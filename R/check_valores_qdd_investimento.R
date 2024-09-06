9659a64ecfb31885685e4488de0767d1cd0a8af4
carloshob <carloshenrique.ob@gmail.com>
2024-09-06 12:22:55 -0300

    ck_valores_qdd_investimento - altera documentação e identação

--- R/check_valores_qdd_investimento.R
+++ R/check_valores_qdd_investimento.R
@@ -5,30 +5,54 @@
 #' para o ano seguinte
 #'
 #' @export
-check_valores_qdd_investimento <- function(base_qdd_investimento, acoes_planejamento, stop_on_failure = FALSE, output = FALSE,
-                                           json_outfile = NULL, log_level = "ERROR",
-                                           msg_template = NULL) {
-  key <- c("uo_cod", "programa_cod", "acao_cod", "funcao_cod", "subfuncao_cod", "iag_cod")
+check_valores_qdd_investimento <- function(base_qdd_investimento,
+                                           acoes_planejamento,
+                                           stop_on_failure = FALSE,
+                                           output = FALSE,
+                                           json_outfile = NULL,
+                                           log_level = "ERROR",
+                                           msg_template = NULL
+                                           ) {
+
+  key <- c("uo_cod",
+           "programa_cod",
+           "acao_cod",
+           "funcao_cod",
+           "subfuncao_cod",
+           "iag_cod"
+           )
 
   x <- base_qdd_investimento |>
-    aggregate("vlr_loa_desp_invest", by = key)
+       aggregate("vlr_loa_desp_invest",
+                 by = key
+                 )
 
   y <- acoes_planejamento |>
-    aggregate("vr_meta_orcamentaria_ano0",
-      by = key,
-      rename = list(uo_acao_cod = "uo_cod"),
-      filter = is_deleted_acao == FALSE & identificador_tipo_acao_cod %in% c(3, 6, 8)
-    )
+       aggregate("vr_meta_orcamentaria_ano0",
+                 by = key,
+                 filter = is_deleted_acao == FALSE &
+                          identificador_tipo_acao_cod %in% c(3, 6, 8),
+                 rename = list(uo_acao_cod = "uo_cod")
+                 )
 
-  df <- merge(x, y, by = key, all = TRUE) |> as_accounting()
+  df <- merge(x, y,
+              by = key,
+              all = TRUE
+              ) |>
+        as_accounting()
 
-  report <- df |> check_that(vlr_loa_desp_invest == vr_meta_orcamentaria_ano0)
+  report <- check_that(df, vlr_loa_desp_invest == vr_meta_orcamentaria_ano0)
   
   default_message = "Foram encontrados erros no teste."
   
   # prioritize the parameter error message if used
   msg_template = msg_template %||% default_message
 
-  check_result(df, report, stop_on_failure = stop_on_failure, output = output,
-               json_outfile = json_outfile, log_level = log_level, msg_template = msg_template)
+  check_result(df, report,
+               stop_on_failure = stop_on_failure,
+               output = output,
+               json_outfile = json_outfile,
+               log_level = log_level,
+               msg_template = msg_template
+               )
 }
