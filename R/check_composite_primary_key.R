531f689d5812d1edff2762afc3ca67c58c8f7f0f
hslinhares <henrique.s.linhares@gmail.com>
2024-09-06 10:11:31 -0300

    cria função de teste específica pra cada base

--- R/check_composite_primary_key.R
+++ R/check_composite_primary_key.R
@@ -1,64 +1,160 @@
 #' @export
-check_composite_primary_key  <- function(siplan){
-  
-  pkey_list <- list(
-    programas = c('uo_programa_cod', 'programa_cod', 'is_deleted_programa', 'objetivo_estrategico_cod', 'diretriz_estrategica_cod', 'ods_titulo'),
-    acoes = c('uo_acao_cod', 'acao_cod', 'is_deleted_acao'),
-    localizadores = c('uo_acao_cod', 'acao_cod', 'is_deleted_acao', 'localizador_cod'),
-    indicadores = c('programa_cod', 'is_deleted_programa', 'indicador', 'is_deleted_indicador')
+check_composite_primary_key_programas <- function(
+    sigplan,
+    stop_on_failure = FALSE,
+    output = TRUE
+){
+  df <- sigplan$programas_planejamento
+  rule <- validate::validator(
+    is_complete(uo_programa_cod, programa_cod, is_deleted_programa, objetivo_estrategico_cod, diretriz_estrategica_cod, ods_titulo),
+    is_unique(uo_programa_cod, programa_cod, is_deleted_programa, objetivo_estrategico_cod, diretriz_estrategica_cod, ods_titulo)
   )
+  report <- validate::confront(df, rule)
   
-  df_list <- list (
-    programas = sigplan$programas_planejamento,
-    acoes = sigplan$acoes_planejamento,
-    localizadores = sigplan$localizadores_todos_planejamento,
-    indicadores = sigplan$indicadores_planejamento
+  default_message = "Linha contém valores ausentes ou duplicados na chave uo_programa_cod = {uo_programa_cod}, programa_cod = {programa_cod}, is_deleted_programa = {is_deleted_programa},
+  objetivo_estrategico_cod = {objetivo_estrategico_cod}, diretriz_estrategica_cod = {diretriz_estrategica_cod}, ods_titulo = {ods_titulo}"
+  
+  # prioritize the parameter error message if used
+  msg_template = msg_template %||% default_message
+  
+  check_result(df, report,
+               stop_on_failure = stop_on_failure,
+               output = output,
+               json_outfile = json_outfile,
+               log_level = log_level,
+               msg_template = msg_template
   )
-  
-  for (name in names(df_list)) {
-    df_list[[name]][, pkey := do.call(paste, c(.SD, sep = "|")), .SDcols = pkey_list[[name]]]
-  }
-
-  rule_list <- list(
-    rule_programas <- validate::validator(
-      is_complete(uo_programa_cod, programa_cod, is_deleted_programa, objetivo_estrategico_cod, diretriz_estrategica_cod, ods_titulo), 
-      is_unique(uo_programa_cod, programa_cod, is_deleted_programa, objetivo_estrategico_cod, diretriz_estrategica_cod, ods_titulo)
-    ),
-    rule_acoes <- validate::validator(
-      is_complete(uo_acao_cod, acao_cod, is_deleted_acao),
-      is_unique(uo_acao_cod, acao_cod, is_deleted_acao)
-    ),
-    rule_localizadores <- validate::validator(
-      is_complete(uo_acao_cod, acao_cod, is_deleted_acao, localizador_cod),
-      is_unique(uo_acao_cod, acao_cod, is_deleted_acao, localizador_cod)
-    ),
-    rule_indicadores <- validate::validator(
-      is_complete(programa_cod, is_deleted_programa, indicador, is_deleted_indicador),
-      is_unique(programa_cod, is_deleted_programa, indicador, is_deleted_indicador)
-    )
-  )
-  
-  report_list <- map2(df_list, rule_list, validate::confront)
-  report_summary_list <- map(report_list, validate::summary)
- 
-  check_validity <- function(df) {
-    isTRUE(all.equal(df$items, df$passes))
-  }
-  
-  valid_list <- map(report_summary_list, check_validity)
-
-  for (name in names(valid_list)) {
-    if (valid_list[[name]] == FALSE) {
-      invalid_rows <- validate::violating(df_list[[name]], report_list[[name]], include_missing = TRUE)
-      invalid_rows <- invalid_rows[,.(pkey)]
-      pkey_columns <- paste(pkey_list[[name]], collapse = ", ")
-      msg <- glue::glue_data(invalid_rows,
-        "A base {name} contém erro na primary-key {pkey_columns}: {pkey}"
-      )
-    }
-  }
-  return(msg)
 }
+
+#' @export
+check_composite_primary_key_acoes <- function(
+    sigplan,
+    stop_on_failure = FALSE,
+    output = TRUE
+){
+  df <- sigplan$programas_planejamento
+  rule <- validate::validator(
+    is_complete(uo_acao_cod, acao_cod, is_deleted_acao),
+    is_unique(uo_acao_cod, acao_cod, is_deleted_acao)
+  )
+  report <- validate::confront(df, rule)
+  
+  default_message = "Linha contém valores ausentes ou duplicados na chave uo_acao_cod = {uo_acao_cod}, acao_cod = {acao_cod}, is_deleted_acao = {is_deleted_acao}"  
+  
+  # prioritize the parameter error message if used
+  msg_template = msg_template %||% default_message
+  
+  check_result(df, report,
+               stop_on_failure = stop_on_failure,
+               output = output,
+               json_outfile = json_outfile,
+               log_level = log_level,
+               msg_template = msg_template
+  )
+}
+
+#' @export
+check_composite_primary_key_localizadores <- function(
+    sigplan,
+    stop_on_failure = FALSE,
+    output = TRUE
+){
+  df <- sigplan$programas_planejamento
+  rule <- validate::validator(
+    is_complete(uo_acao_cod, acao_cod, is_deleted_acao, localizador_cod),
+    is_unique(uo_acao_cod, acao_cod, is_deleted_acao, localizador_cod)
+  )
+  report <- validate::confront(df, rule)
+  
+  default_message = "Linha contém valores ausentes ou duplicados na chave uo_acao_cod = {uo_acao_cod}, acao_cod = {acao_cod}, is_deleted_acao = {is_deleted_acao},
+  localizador_cod = {localizador_cod}"
+  
+  # prioritize the parameter error message if used
+  msg_template = msg_template %||% default_message
+  
+  check_result(df, report,
+               stop_on_failure = stop_on_failure,
+               output = output,
+               json_outfile = json_outfile,
+               log_level = log_level,
+               msg_template = msg_template
+  )
+}
+
+#' @export
+check_composite_primary_key_indicadores <- function(
+    sigplan,
+    stop_on_failure = FALSE,
+    output = TRUE
+){
+  df <- sigplan$programas_planejamento
+  rule <- validate::validator(
+    is_complete(programa_cod, is_deleted_programa, indicador, is_deleted_indicador),
+    is_unique(programa_cod, is_deleted_programa, indicador, is_deleted_indicador)
+  )
+  report <- validate::confront(df, rule)
+  
+  default_message = "Linha contém valores ausentes ou duplicados na chave programa_cod = {programa_cod}, programa_cod = {programa_cod}, 
+  is_deleted_programa = {is_deleted_programa}, indicador = {indicador}, is_deleted_indicador = {is_deleted_indicador}"
+  # prioritize the parameter error message if used
+  msg_template = msg_template %||% default_message
+  
+  check_result(df, report,
+               stop_on_failure = stop_on_failure,
+               output = output,
+               json_outfile = json_outfile,
+               log_level = log_level,
+               msg_template = msg_template
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
