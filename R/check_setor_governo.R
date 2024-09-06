a7db68481f51090b562e211ccef3e724cd00b64f
carloshob <carloshenrique.ob@gmail.com>
2024-09-05 21:36:03 -0300

    ck_setor_governos - altera mensagem do check

--- R/check_setor_governo.R
+++ R/check_setor_governo.R
@@ -31,7 +31,11 @@ check_setor_governo <- function(acoes_planejamento,
 
   report <- check_that(df, setor_governo == setor_governo_referencia)
   
-  default_message = "Foram encontrados erros no teste."
+  default_message = paste0(
+                    "A uo {uo_acao_cod} está com setor divergente na base ações-planejamento ",
+                    "({setor_governo}) em relação à tabela auxiliar setor-governo da loa ",
+                    "({setor_governo_referencia})."
+                    )
   
   # prioritize the parameter error message if used
   msg_template = msg_template %||% default_message
