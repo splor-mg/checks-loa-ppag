d09d46fe274e23a6385d8d4cc0612b30a8fa700f
carloshob <carloshenrique.ob@gmail.com>
2024-09-06 12:13:14 -0300

    ck_valores_qdd_fiscal - altera mensagem do check

--- R/check_valores_qdd_fiscal.R
+++ R/check_valores_qdd_fiscal.R
@@ -5,26 +5,49 @@
 #' para o ano seguinte
 #'
 #' @export
-check_valores_qdd_fiscal <- function(base_qdd_fiscal, acoes_planejamento, stop_on_failure = FALSE, output = FALSE, 
-                                     json_outfile = NULL, log_level = "ERROR",
-                                     msg_template = NULL) {
+check_valores_qdd_fiscal <- function(base_qdd_fiscal,
+                                     acoes_planejamento,
+                                     stop_on_failure = FALSE,
+                                     output = FALSE, 
+                                     json_outfile = NULL,
+                                     log_level = "ERROR",
+                                     msg_template = NULL
+                                     ) {
   
-  key <- c("uo_cod", "programa_cod", "acao_cod", "funcao_cod", "subfuncao_cod", "iag_cod")
+  key <- c("uo_cod",
+           "programa_cod",
+           "acao_cod",
+           "funcao_cod",
+           "subfuncao_cod",
+           "iag_cod"
+           )
 
   x <- base_qdd_fiscal |>
-    aggregate("vlr_loa_desp$", by = key)
+       aggregate("vlr_loa_desp$",
+                 by = key
+                 )
 
   y <- acoes_planejamento |>
-    aggregate("vr_meta_orcamentaria_ano0",
-      by = key,
-      filter = is_deleted_acao == FALSE & identificador_tipo_acao_cod %in% c(1, 2, 4, 7, 9),
-      rename = list(uo_acao_cod = "uo_cod")
-    )
+       aggregate("vr_meta_orcamentaria_ano0",
+                 by = key,
+                 filter = is_deleted_acao == FALSE &
+                          identificador_tipo_acao_cod %in% c(1, 2, 4, 7, 9),
+                 rename = list(uo_acao_cod = "uo_cod")
+                 )
 
-  df <- merge(x, y, by = key, all = TRUE) |> as_accounting()
-  report <- df |> check_that(vlr_loa_desp == vr_meta_orcamentaria_ano0)
+  df <- merge(x, y,
+              by = key,
+              all = TRUE
+              ) |>
+        as_accounting()
+ 
+  report <- check_that(df, vlr_loa_desp == vr_meta_orcamentaria_ano0)
   
-  default_message = "Funcional programática {programa_cod}.{acao_cod}.{funcao_cod}.{subfuncao_cod} da UO {uo_cod} com valor na base QDD Fiscal ({vlr_loa_desp}) diferente da base Açoes Planejamento do SIGPLAN ({vr_meta_orcamentaria_ano0}"
+  default_message = paste0(
+                    "Funcional programática {programa_cod}.{acao_cod}.{funcao_cod}.{subfuncao_cod} ",
+                    "da uo {uo_cod} com valor na base qdd-fiscal ({vlr_loa_desp}) diferente da base ",
+                    "ações-planejamento ({vr_meta_orcamentaria_ano0},"
+                    )
   
   msg_template = msg_template %||% default_message
   
@@ -33,5 +56,11 @@ check_valores_qdd_fiscal <- function(base_qdd_fiscal, acoes_planejamento, stop_o
   # prioritize the parameter error message if used
   msg_template = msg_template %||% default_message
   
-  check_result(df, report, stop_on_failure = stop_on_failure, output = output, json_outfile = json_outfile, log_level = log_level, msg_template = msg_template)
+  check_result(df, report,
+               stop_on_failure = stop_on_failure,
+               output = output,
+               json_outfile = json_outfile,
+               log_level = log_level,
+               msg_template = msg_template
+               )
 }
