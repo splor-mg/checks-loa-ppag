9ab270d2c0b8ff262b2fe2810b94dfa3e93bed11
carloshob <carloshenrique.ob@gmail.com>
2024-09-06 21:29:19 -0300

    ck_valores_sigplan_programas - altera documentação e identação

--- R/check_valores_sigplan_programas.R
+++ R/check_valores_sigplan_programas.R
@@ -20,46 +20,142 @@
 #' - identificador de procedência e uso (IPU)
 #'  
 #' @export
-check_valores_sigplan_programas <- function(acoes_planejamento, programas_planejamento, stop_on_failure = FALSE, output = FALSE,
-                                            json_outfile = NULL, log_level = "ERROR",
-                                            msg_template = NULL) {
-  key <- c("programa_cod", "area_tematica_cod", "uo_programa_cod")
+check_valores_sigplan_programas <- function(acoes_planejamento,
+                                            programas_planejamento,
+                                            stop_on_failure = FALSE,
+                                            output = FALSE,
+                                            json_outfile = NULL,
+                                            log_level = "ERROR",
+                                            msg_template = NULL
+                                            ) {
+  
+  key <- c("programa_cod",
+           "area_tematica_cod",
+           "uo_programa_cod"
+           )
   
   x <- acoes_planejamento |> 
-    aggregate("vr_meta_orcamentaria_ano", 
-              by = key,
-              filter = is_deleted_programa == FALSE & is_deleted_acao == FALSE,
-              rename = list(
-                vr_meta_orcamentaria_ano0 = "vr_meta_orcamentaria_ano0_acoes",
-                vr_meta_orcamentaria_ano1 = "vr_meta_orcamentaria_ano1_acoes",
-                vr_meta_orcamentaria_ano2 = "vr_meta_orcamentaria_ano2_acoes",
-                vr_meta_orcamentaria_ano3 = "vr_meta_orcamentaria_ano3_acoes"
-              ))
-  
+       aggregate("vr_meta_orcamentaria_ano", 
+                 by = key,
+                 filter = is_deleted_programa == FALSE &
+                          is_deleted_acao == FALSE,
+                 rename = list(
+                               vr_meta_orcamentaria_ano0 = "vr_meta_orcamentaria_ano0_acoes",
+                               vr_meta_orcamentaria_ano1 = "vr_meta_orcamentaria_ano1_acoes",
+                               vr_meta_orcamentaria_ano2 = "vr_meta_orcamentaria_ano2_acoes",
+                               vr_meta_orcamentaria_ano3 = "vr_meta_orcamentaria_ano3_acoes"
+                               )
+                 )
+    
   y <- programas_planejamento |> 
        dplyr::filter(is_deleted_programa == FALSE) |> 
        unique(by = c("uo_programa_cod", "programa_cod")) |> 
-    aggregate("vr_meta_orcamentaria_ano", 
+       aggregate("vr_meta_orcamentaria_ano", 
+                 by = key,
+                 rename = list(
+                               vr_meta_orcamentaria_ano0 = "vr_meta_orcamentaria_ano0_programas",
+                               vr_meta_orcamentaria_ano1 = "vr_meta_orcamentaria_ano1_programas",
+                               vr_meta_orcamentaria_ano2 = "vr_meta_orcamentaria_ano2_programas",
+                               vr_meta_orcamentaria_ano3 = "vr_meta_orcamentaria_ano3_programas"
+                               )
+                 )
+  
+  df <- merge(x, y,
               by = key,
-              rename = list(
-                vr_meta_orcamentaria_ano0 = "vr_meta_orcamentaria_ano0_programas",
-                vr_meta_orcamentaria_ano1 = "vr_meta_orcamentaria_ano1_programas",
-                vr_meta_orcamentaria_ano2 = "vr_meta_orcamentaria_ano2_programas",
-                vr_meta_orcamentaria_ano3 = "vr_meta_orcamentaria_ano3_programas"
-              ))
+              all = TRUE
+              ) |>
+        as_accounting()
+
+  report <- check_that(df, 
+                       vr_meta_orcamentaria_ano0_acoes == vr_meta_orcamentaria_ano0_programas,
+                       vr_meta_orcamentaria_ano1_acoes == vr_meta_orcamentaria_ano1_programas,
+                       vr_meta_orcamentaria_ano2_acoes == vr_meta_orcamentaria_ano2_programas,
+                       vr_meta_orcamentaria_ano3_acoes == vr_meta_orcamentaria_ano3_programas
+                       )
   
-  df <- merge(x, y, by = key, all = TRUE) |> as_accounting()
-  report <- df |> check_that(
-    vr_meta_orcamentaria_ano0_acoes == vr_meta_orcamentaria_ano0_programas,
-    vr_meta_orcamentaria_ano1_acoes == vr_meta_orcamentaria_ano1_programas,
-    vr_meta_orcamentaria_ano2_acoes == vr_meta_orcamentaria_ano2_programas,
-    vr_meta_orcamentaria_ano3_acoes == vr_meta_orcamentaria_ano3_programas)
-  
-  default_message = "Foram encontrados erros no teste."
+  default_message = "A uo {uo_programa_cod}, 
+        programa {programa_cod}, 
+        área temática {area_tematica_cod} está com valor 
+        {ifelse(
+            vr_meta_orcamentaria_ano0_acoes != vr_meta_orcamentaria_ano0_programas | 
+            is.na(vr_meta_orcamentaria_ano0_acoes) | 
+            is.na(vr_meta_orcamentaria_ano0_programas), 
+            paste0('de meta orçamentária do ano 0, base programas (R$ ', 
+                   vr_meta_orcamentaria_ano0_programas, 
+                   ') diferente da base ações-planejamento (R$ ', 
+                   vr_meta_orcamentaria_ano0_acoes, 
+                   '). Diferença de R$ ', 
+                   ifelse(is.na(vr_meta_orcamentaria_ano0_acoes) & 
+                          is.na(vr_meta_orcamentaria_ano0_programas), 
+                          'NA', 
+                          ifelse(is.na(vr_meta_orcamentaria_ano0_acoes), 
+                                 vr_meta_orcamentaria_ano0_programas, 
+                                 ifelse(is.na(vr_meta_orcamentaria_ano0_programas), 
+                                        vr_meta_orcamentaria_ano0_acoes, 
+                                        vr_meta_orcamentaria_ano0_acoes - 
+                                        vr_meta_orcamentaria_ano0_programas))), '.'), 
+            ifelse(
+                vr_meta_orcamentaria_ano1_acoes != vr_meta_orcamentaria_ano1_programas | 
+                is.na(vr_meta_orcamentaria_ano1_acoes) | 
+                is.na(vr_meta_orcamentaria_ano1_programas), 
+                paste0('de meta orçamentária do ano 1, base programas (R$ ', 
+                       vr_meta_orcamentaria_ano1_programas, 
+                       ') diferente da base ações-planejamento (R$ ', 
+                       vr_meta_orcamentaria_ano1_acoes, 
+                       '). Diferença de R$ ', 
+                       ifelse(is.na(vr_meta_orcamentaria_ano1_acoes) & 
+                              is.na(vr_meta_orcamentaria_ano1_programas), 
+                              'NA', 
+                              ifelse(is.na(vr_meta_orcamentaria_ano1_acoes), 
+                                     vr_meta_orcamentaria_ano1_programas, 
+                                     ifelse(is.na(vr_meta_orcamentaria_ano1_programas), 
+                                            vr_meta_orcamentaria_ano1_acoes, 
+                                            vr_meta_orcamentaria_ano1_acoes - 
+                                            vr_meta_orcamentaria_ano1_programas))), '.'), 
+                ifelse(
+                    vr_meta_orcamentaria_ano2_acoes != vr_meta_orcamentaria_ano2_programas | 
+                    is.na(vr_meta_orcamentaria_ano2_acoes) | 
+                    is.na(vr_meta_orcamentaria_ano2_programas), 
+                    paste0('de meta orçamentária do ano 2, base programas (R$ ', 
+                           vr_meta_orcamentaria_ano2_programas, 
+                           ') diferente da base ações-planejamento (R$ ', 
+                           vr_meta_orcamentaria_ano2_acoes, 
+                           '). Diferença de R$ ', 
+                           ifelse(is.na(vr_meta_orcamentaria_ano2_acoes) & 
+                                  is.na(vr_meta_orcamentaria_ano2_programas), 
+                                  'NA', 
+                                  ifelse(is.na(vr_meta_orcamentaria_ano2_acoes), 
+                                         vr_meta_orcamentaria_ano2_programas, 
+                                         ifelse(is.na(vr_meta_orcamentaria_ano2_programas), 
+                                                vr_meta_orcamentaria_ano2_acoes, 
+                                                vr_meta_orcamentaria_ano2_acoes - 
+                                                vr_meta_orcamentaria_ano2_programas))), '.'), 
+                      paste0('de meta orçamentária do ano 3, base programas (R$ ', 
+                             vr_meta_orcamentaria_ano3_programas, 
+                             ') diferente da base ações-planejamento (R$ ', 
+                             vr_meta_orcamentaria_ano3_acoes, 
+                             '). Diferença de R$ ', 
+                             ifelse(is.na(vr_meta_orcamentaria_ano3_acoes) & 
+                                    is.na(vr_meta_orcamentaria_ano3_programas), 
+                                    'NA', 
+                                    ifelse(is.na(vr_meta_orcamentaria_ano3_acoes), 
+                                           vr_meta_orcamentaria_ano3_programas, 
+                                           ifelse(is.na(vr_meta_orcamentaria_ano3_programas), 
+                                                  vr_meta_orcamentaria_ano3_acoes, 
+                                                  vr_meta_orcamentaria_ano3_acoes - 
+                                                  vr_meta_orcamentaria_ano3_programas))), '.')
+                )
+            )
+        )}"
   
   # prioritize the parameter error message if used
   msg_template = msg_template %||% default_message
   
-  check_result(df, report, stop_on_failure = stop_on_failure, output = output,
-               json_outfile = json_outfile, log_level = log_level, msg_template = msg_template)
+  check_result(df, report,
+               stop_on_failure = stop_on_failure,
+               output = output,
+               json_outfile = json_outfile,
+               log_level = log_level,
+               msg_template = msg_template
+               )
 }
