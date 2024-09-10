44da54d97c8687a821a28b9104dca538d70740eb
carloshob <carloshenrique.ob@gmail.com>
2024-09-09 22:45:31 -0300

    ck_detalhamento_obras_orcam_investimento - altera mensagem do check

--- R/check_detalhamento_obras_orcam_investimento.R
+++ R/check_detalhamento_obras_orcam_investimento.R
@@ -42,9 +42,10 @@ check_detalhamento_obras_orcam_investimento <- function(base_qdd_investimento,
   report <- df |> check_that(vlr_loa_desp_invest == vlr_outros_ano0)
   
   default_message = paste0(
-                    "A ação {acao_cod}, na UO {uo_cod}, função-subfunção {funcao_cod}-{subfuncao_cod}, ",
-                    "programa {programa_cod} e iag {iag_cod} está com valores diferentes entre as bases, ",
-                    "R$ {vlr_loa_desp_invest} na qdd investimento e R$ {vlr_outros_ano0} na detalhamento de obras."
+                    "A ação {acao_cod}, na uo {uo_cod}, funcional-programática {sprintf('%02d', funcao_cod)}.",
+                    "{sprintf('%03d', subfuncao_cod)}.{sprintf('%03d', programa_cod)}, iag {iag_cod}, está com ",
+                    "valores diferentes entre as bases, R$ {vlr_loa_desp_invest} na qdd-investimento e R$ ",
+                    "{vlr_outros_ano0} na detalhamento de obras."
                     )
   
   # prioritize the parameter error message if used
