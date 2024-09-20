f734bb942c56036f67f3806f14c5bb68a970ce5e
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2024-09-20 16:31:08 -0300

    refactor: passa data.frame como argumento no lugar de lista

--- R/check_composite_primary_key.R
+++ R/check_composite_primary_key.R
@@ -1,13 +1,13 @@
 #' @export
 check_composite_primary_key_programas <- function(
-    sigplan,
+    programas_planejamento,
     stop_on_failure = FALSE,
     output = TRUE,
     json_outfile = NULL, 
     log_level = "ERROR",
     msg_template = NULL
 ){
-  df <- sigplan$programas_planejamento
+  df <- programas_planejamento
   rule <- validate::validator(
     is_complete(uo_programa_cod, programa_cod, is_deleted_programa, objetivo_estrategico_cod, diretriz_estrategica_cod, ods_titulo),
     is_unique(uo_programa_cod, programa_cod, is_deleted_programa, objetivo_estrategico_cod, diretriz_estrategica_cod, ods_titulo)
@@ -32,14 +32,14 @@ check_composite_primary_key_programas <- function(
 
 #' @export
 check_composite_primary_key_acoes <- function(
-    sigplan,
+    acoes_planejamento,
     stop_on_failure = FALSE,
     output = TRUE,
     json_outfile = NULL, 
     log_level = "ERROR",
     msg_template = NULL
 ){
-  df <- sigplan$acoes_planejamento
+  df <- acoes_planejamento
   rule <- validate::validator(
     is_complete(uo_acao_cod, acao_cod, is_deleted_acao),
     is_unique(uo_acao_cod, acao_cod, is_deleted_acao)
@@ -62,14 +62,14 @@ check_composite_primary_key_acoes <- function(
 
 #' @export
 check_composite_primary_key_localizadores <- function(
-    sigplan,
+    localizadores_todos_planejamento,
     stop_on_failure = FALSE,
     output = TRUE,
     json_outfile = NULL, 
     log_level = "ERROR",
     msg_template = NULL
 ){
-  df <- sigplan$localizadores_todos_planejamento
+  df <- localizadores_todos_planejamento
   rule <- validate::validator(
     is_complete(uo_acao_cod, acao_cod, is_deleted_acao, localizador_cod),
     is_unique(uo_acao_cod, acao_cod, is_deleted_acao, localizador_cod)
@@ -93,14 +93,14 @@ check_composite_primary_key_localizadores <- function(
 
 #' @export
 check_composite_primary_key_indicadores <- function(
-    sigplan,
+    indicadores_planejamento,
     stop_on_failure = FALSE,
     output = TRUE,
     json_outfile = NULL, 
     log_level = "ERROR",
     msg_template = NULL
 ){
-  df <- sigplan$indicadores_planejamento
+  df <- indicadores_planejamento
   rule <- validate::validator(
     is_complete(programa_cod, is_deleted_programa, indicador, is_deleted_indicador),
     is_unique(programa_cod, is_deleted_programa, indicador, is_deleted_indicador)
